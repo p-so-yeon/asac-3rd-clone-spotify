f786c4bc8df50aad036dd2273e55f7ca39ec3f2f
Jo Seung Hyo <seunghyo7742@naver.com>
2023-08-27 22:23:54 +0900

    Feat: add elements

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -4,7 +4,8 @@ import { usePathname } from 'next/navigation';
 import { useMemo } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { GoHome } from 'react-icons/go';
-
+import { BiLibrary } from 'react-icons/bi';
+import {AiOutlinePlus} from 'react-icons/ai';
 function Sidebar() {
   const pathname = usePathname();
   const routes = useMemo(
@@ -25,17 +26,45 @@ function Sidebar() {
     [pathname]
   );
   return (
-    <div className="flex h-full">
-      <div className="bg---color-box-primary rounded-lg h-fit w-full">
+    <div className="flex flex-col h-full bg-black">
+      <div className="bg-teal-950 rounded-lg h-fit w-full">
         <div>
           {routes.map((item) => (
-            <Link key={item.label} href={item.href} className="active:text-white">
-              <item.icon size={26} />
+            <Link
+              key={item.label}
+              href={item.href}
+              className="active:text-white text-neutral-300"
+            >
+              <item.icon className="" size={26} />
               <p className="w-full">{item.label}</p>
             </Link>
           ))}
         </div>
       </div>
+      <main className="bg-teal-950 rounded-lg h-fit w-full ">
+        {/* Library header */}
+        <div>
+          <div className="flex items-center justify-between">
+            <div className="inline-flex items-center gap-x-2">
+              <BiLibrary className="text-neutral-300" size={26}/>
+              <p className="font-medium text-neutral-300 text-md">내 라이브러리</p>
+            </div>
+            <AiOutlinePlus  size={20} className="text-neutral-300 hover:text-white cursor-pointer transition"/>
+          </div>
+          <div className="flex items-center justify-between">
+            <BiSearch className="text-neutral-300"/>
+            <p className="text-neutral-300"> Recents</p>
+          </div>
+        </div>
+        {/*Library list*/}
+        <div className="h-full overflow-y-auto">
+            <div className="h-20">Example list</div>
+            <div className="h-20">Example list</div>
+            <div className="h-20">Example list</div>
+
+        </div>
+        
+      </main>
     </div>
   );
 }

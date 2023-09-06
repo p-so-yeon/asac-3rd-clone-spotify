9946202fb3dacc7b35aea73449876fe8c637942f
soyeon <soyeon1056@naver.com>
2023-09-06 23:18:47 +0900

    feat:current_user mock data

---
+++ src/mock/api/data/current_user-data.ts
@@ -0,0 +1,30 @@
+export const CURRENT_USER_DATA = {}
+
+export const CUR_USER_DATA = {
+  country: 'string',
+  display_name: 'string',
+  email: 'string',
+  explicit_content: {
+    filter_enabled: false,
+    filter_locked: false,
+  },
+  "external_urls" : {
+    spotify: 'string',
+  },
+  "followers: {
+    href: 'string',
+    total: 0,
+  },
+  href: 'string',
+  id: 'string',
+  images: [
+    {
+      url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+      height: 300,
+      width: 300,
+    },
+  ],
+  product: 'string',
+  type: 'string',
+  uri: 'string',
+}

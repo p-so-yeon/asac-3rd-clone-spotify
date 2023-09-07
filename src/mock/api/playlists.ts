19caea8ab8bd88e27a0c80a9a90d0c2d0d74cdb4
Jo Seung Hyo <seunghyo7742@naver.com>
2023-09-07 10:58:37 +0900

    Chore: Add feautred-data

--- src/mock/api/playlists.ts
+++ src/mock/api/playlists.ts
@@ -2,9 +2,11 @@ import { rest } from "msw"
 
 import { MOCK_API_URL } from "@/lib/constant/path"
 import { PLAYLIST_DATA } from "@/mock/api/data/playlists-data"
+import {FEATURED_PLAYLIST_DATA} from "@/mock/api/data/playlists-data"
 
 const PlayListsHandler = [
 
   rest.get(`${MOCK_API_URL}/recent-playlist`, (_, res, ctx) => res(ctx.json(PLAYLIST_DATA))),
+  rest.get(`${MOCK_API_URL}/featured-playlist`,(_,res,ctx)=> res(ctx.json(FEATURED_PLAYLIST_DATA))),
 ]
 export default PlayListsHandler

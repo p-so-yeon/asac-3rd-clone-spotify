76b8e765cdadd40381f379a4ed6eb6da395fc82e
Jo Seung Hyo <seunghyo7742@naver.com>
2023-09-22 01:20:13 +0900

    CHORE:컴포넌트 분리  리팩토링

--- src/app/playlist/SearchTrack.tsx
+++ src/app/playlist/SearchTrack.tsx
@@ -7,9 +7,11 @@ export default function SearchTrack() {
   function search(input) {}
 
   return (
-    <div>
-      <h1 className="text-white text-xl font-bold ">플레이리스트에 추가할 곡을 찾아보세요</h1>
-      <SearchBar onChange={search} placeholder="곡 또는 에피소드 검색하기" />
+    <div >
+      <h1 className="text-white text-xl font-bold mb-4 ">플레이리스트에 추가할 곡을 찾아보세요</h1>
+      <div className="w-2/5">
+        <SearchBar onChange={search} placeholder="곡 또는 에피소드 검색하기" />
+      </div>
     </div>
   )
 }
--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -1,6 +1,6 @@
+import PlaylistInfo from '@/app/playlist/PlaylistInfo'
+import Playlists from '@/app/playlist/Playlists'
 import SearchTrack from '@/app/playlist/SearchTrack'
-import Image from 'next/image'
-import { SlOptions } from 'react-icons/sl'
 
 const test_data = {
   cover_img: '',
@@ -11,42 +11,17 @@ const test_data = {
 
 export default function page() {
   return (
-    <div className="bg-color-background-base-primary h-full">
-      <section className="relative px-4 flex w-full h-72 bg-gradient-to-b from-transparent to-color-card-primary">
-        <button className="shadow-xl mr-5 relative top-16 text-white w-52 h-52 hover:bg-color-hover-primary">
-          <Image
-            fill={true}
-            src={test_data.cover_img ? test_data.cover_img : '/img/playlistDefault.png'}
-            alt="playlist cover image"
-          />
-        </button>
-        <div className="text-white pt-36 flex flex-col gap-4  ">
-          <span className="text-sm">플레이리스트</span>
-          <button className="text-5xl font-extrabold">{test_data.title}</button>
-          <div className="flex gap-2">
-            <div className=" overflow-hidden rounded-full relative w-6 h-6">
-              <Image
-                fill={true}
-                src={test_data.author_profile ? test_data.author_profile : '/img/userDefaultProfile.jpeg'}
-                alt="author profile image"
-              />
-            </div>
-            <p>{test_data.author}</p>
-          </div>
-        </div>
-      </section>
+    <div className="h-full">
+      <PlaylistInfo test_data={test_data} />
       {/*Tracks */}
-      <div>
-        <div className="w-full min-h-14 h-14 px-6 py-6 ">
-          <SlOptions size={24} className="text-color-text-secondary" />
-        </div>
-        <div>
-          <div className="bg-slate-400 w-full h-24"></div>
-        </div>
+      <div className="px-5">
+        <section>
+          <Playlists />
+        </section>
+        <section className="h-52 mt-4 pt-4 border-t border-neutral-500">
+          <SearchTrack />
+        </section>
       </div>
-      <section>
-        <SearchTrack/>
-      </section>
     </div>
   )
 }

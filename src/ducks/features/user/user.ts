1f499a1498ecc3baffdb0106d46fee29033474e7
HyunSJ <dudtod1596@gmail.com>
2023-09-21 10:52:35 +0900

    feat : auth, libraray, player, user slice 추가 미완성

---
+++ src/ducks/features/user/user.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+import { Image } from "@/types/common/images-data-type";
+
+interface userProfile {
+  //타입 작성
+  name: string;
+  followers: number
+  profileImage: Image | null
+}
+
+const initialState: userProfile = {
+  name: "",
+  followers: 0,
+  profileImage: null,
+}
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    // pauseTrack: (state, action: PayloadAction<boolean>) => {
+    //   state.isPlaying = action.payload
+    // },
+  },
+  extraReducers: {}
+})
+
+export const { } = userSlice.actions
+export default userSlice.reducer
